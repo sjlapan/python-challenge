cfd6b38534b34cb1b03c86b5ba0b82e1730d6435
Esjael <stewartjlapan@gmail.com>
2019-03-11 14:06:45 -0600

    Debugged Max and Min functions and altered printouts

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,29 +15,35 @@ with open (budget_csv, newline = "") as budget:
     header = next(budget)
     for row in reader:
         rows.append(row)
-    # Check the number of months stored in rows at the end of the loop
-    print(f"There are {len(rows)} months in the table.")
 
-# Check to make sure the headers were stored properly
-print(header)
-print(rows)
+# Check the number of months stored in rows at the end of the loop
+print("Financial Analysis")
+print("------------------------")
+print(f"Total Months: {len(rows)}")
+
 
 # Calculate net profit change
 for month in rows:
     net_change += int(month[1])
-print(net_change)
+print(f"Total: ${net_change}")
 
 # Get the maximum and minimum profit/losses values
 def max_profit(list):
     maximum = list[0]
     for row in list:
-        if row[1] > maximum[1]
+        if row[1] > maximum[1]:
             maximum = row
     print(f"Greatest Increase in Profits: {maximum} ")
+    return maximum
 
 def min_profit(list):
     minimum = list[0]
     for row in list:
-        if row[1] < minimum[1]
+        if row[1] < minimum[1]:
             minimum = row
     print(f"Greatest Decrease in Profits: {minimum} ")
+    return minimum
+
+#
+max_profit(rows)
+min_profit(rows)
