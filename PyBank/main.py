a24a463fcd5437ca23cc363eb00842c0f25f6877
Esjael <stewartjlapan@gmail.com>
2019-03-11 10:29:55 -0600

    Began code for interpreting .csv file in Python

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,4 +3,7 @@ import os
 
 budget_csv = os.path.join("D:/DU_Bootcamp/code/python-challenge/PyBank", "budget_data.csv")
 
-# 
+with open (budget_csv, newline = "") as budget:
+    reader = csv.reader(budget, delimiter = ",")
+    header = next(budget)
+     
