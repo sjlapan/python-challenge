b55022717f93c6ee691ab833a0974a056117ba46
Esjael <stewartjlapan@gmail.com>
2019-03-12 10:49:25 -0600

    Finished extracting the print statement, though my months for the max and min values are one off

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,24 +15,23 @@ with open (budget_csv, newline = "") as budget:
     header = next(budget)
     
     for row in reader:
-        profit_loss.append(float(row[1]),
+        profit_loss.append(float(row[1])),
         date.append(row[0])
-    print("Financial Analysis")
+    print ("Financial Analysis")
     print("------------------------")
     print(f"Total Months: {len(date)}")
-    print(f"Total: {sum(profit_loss)}")
-
+    print(f"Total: ${int(sum(profit_loss))}")
     for i in range(1, len(profit_loss)):
-        profit_loss_change.append(profit_loss[i] - profit_loss[i - 1])
+        profit_loss_change.append(profit_loss[i] - profit_loss[i-1])
     avg_change = sum(profit_loss_change)/len(profit_loss_change)
     min_change = min(profit_loss_change)
     max_change = max(profit_loss_change)
     #store the dates for the max and min in the dates list using index of max and min
     max_date = str(date[profit_loss_change.index(max(profit_loss_change))])
     min_date = str(date[profit_loss_change.index(min(profit_loss_change))])
-    print(f"Average Change: ${avg_change}")
-    print(f"Greatest Increase in Profits: {max_date} (${max_change})")
-    print(f"Greatest Decrease in Profits: {min_date} (${min_change})")
+    print(f"Average Change: ${round(avg_change, 2)}")
+    print(f"Greatest Increase in Profits: {max_date} (${int(max_change)})")
+    print(f"Greatest Decrease in Profits: {min_date} (${int(min_change)})")
 
 
 
