f331e46f3ded3a1ce78a4960d27657094df7489c
Esjael <stewartjlapan@gmail.com>
2019-03-13 16:35:11 -0600

    finished first version of vote_counter function

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,6 +12,17 @@ os.chdir("D:/DU_Bootcamp/code/python-challenge/PyPoll")
 
 poll_csv = os.path.join("D:/DU_Bootcamp/code/python-challenge/PyPoll", "election_data.csv")
 
+# Create function to tally votes
+
+def vote_counter(candidate_index, vote_list):
+    # @TODO Trying to automatically extract values of candidate list
+    # to use as variables set to integer values...
+    tally = dict.fromkeys(candidate_index, 0)
+    for vote in vote_list:
+        for i in tally:
+            if i == vote:
+                i += 1
+    print(tally)
 
 
 
@@ -19,16 +30,20 @@ with open(poll_csv, newline = "") as poll_data:
     reader = csv.reader(poll_data, delimiter = ",")
     header = next(poll_data)
     # Extract elements for analysis to empty lists
+    # @TODO voter_list is redundant, remove
     voter_list = []
     candidate_list = []
+    total_vote = []
     # Collect all unique candidates and voter IDs
     for row in reader:
         voter_list.append(row[0])
-        if str(row[2]) not in candidate_list:
-            candidate_list.append(str(row[2]))
+        total_vote.append(row[2])
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
             
 
 print(f"There are {len(voter_list)} unique voters.")
 print(f"There are {len(candidate_list)} candidates.")
 print(candidate_list)
+print(f"There are {len(total_vote)} votes cast.")
        
