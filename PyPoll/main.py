c256816b62397dcbcc91f225e70aa6a3e95da5a5
Esjael <stewartjlapan@gmail.com>
2019-03-13 15:29:01 -0600

    Wrote code to store unique candidate values and unique voter values, code returning integers for forme and no values for latter

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,3 +14,18 @@ poll_csv = os.path.join("D:/DU_Bootcamp/code/python-challenge/PyPoll", "election
 
 with open(poll_csv, newline = "") as poll_data:
     reader = csv.reader(poll_data, delimiter = ",")
+    header = next(poll_data)
+    # Extract elements for analysis
+    voter_list = []
+    candidate_list = []
+    # Collect all unique candidates and voter IDs
+    for row in poll_data:
+        if str(row[2]) not in candidate_list:
+            candidate_list.append(str(row[2]))
+    for row in poll_data:
+        if str(row[0]) not in voter_list:
+            voter_list.append(str(row[0]))
+
+print(f"There are {len(voter_list)} unique votes.")
+print(f"There are {len(candidate_list)} candidates.")
+print(candidate_list)        
