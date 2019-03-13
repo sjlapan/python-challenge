dd2a518493ec0bc4bccc8172fe11b686c824b1b1
Esjael <stewartjlapan@gmail.com>
2019-03-13 15:43:33 -0600

    Fixed for loops and got appropriate values for candidates, still getting no values for voter IDs

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,20 +12,24 @@ os.chdir("D:/DU_Bootcamp/code/python-challenge/PyPoll")
 
 poll_csv = os.path.join("D:/DU_Bootcamp/code/python-challenge/PyPoll", "election_data.csv")
 
+
+
+
 with open(poll_csv, newline = "") as poll_data:
     reader = csv.reader(poll_data, delimiter = ",")
     header = next(poll_data)
-    # Extract elements for analysis
+    # Extract elements for analysis to empty lists
     voter_list = []
     candidate_list = []
     # Collect all unique candidates and voter IDs
-    for row in poll_data:
+    for row in reader:
         if str(row[2]) not in candidate_list:
             candidate_list.append(str(row[2]))
-    for row in poll_data:
+    for row in reader:
         if str(row[0]) not in voter_list:
             voter_list.append(str(row[0]))
 
 print(f"There are {len(voter_list)} unique votes.")
 print(f"There are {len(candidate_list)} candidates.")
-print(candidate_list)        
+print(candidate_list)
+        
